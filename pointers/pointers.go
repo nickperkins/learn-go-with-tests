8d9f6bc96c82f397e9375e82973d83f6bf1b487e
Nick Perkins <n.perkins@arkoselabs.com>
2020-11-17 11:33:01 +1000

    pointers add comments

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -5,30 +5,32 @@ import (
 	"fmt"
 )
 
+// A Bitcoin type
 type Bitcoin int
-
-type Stringer interface {
-	String() string
-}
 
 func (b Bitcoin) String() string {
 	return fmt.Sprintf("%d BTC", b)
 }
 
+// A Wallet to hold all the bitcoins
 type Wallet struct {
 	balance Bitcoin
 }
 
+// Deposit some bitcoins
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
+// Balance of your wallet
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
+// ErrInsufficientFunds error
 var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
 
+// Withdrawl of bitcoints
 func (w *Wallet) Withdrawl(amount Bitcoin) error {
 
 	if amount > w.balance {
